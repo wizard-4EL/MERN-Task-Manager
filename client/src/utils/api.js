dbdd2bb4b832768487e25914f2ad78bef90e29c5
Klenamm <harport361@gmail.com>
2026-05-17 16:25:47 +0000

    API Client call updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-task-manager-1agl.onrender.com/api',
 });
 
 // Add token to requests
